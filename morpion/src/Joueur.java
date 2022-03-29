bcd6721ff040f914b2ab78387731ff9923b1bcf5
armelito60 <tchiasso@et.esiea.fr>
2022-03-29 21:46:18 +0200

    tirage au sort des joueurs (IA ou humain) et creation methode aiplay et humanplay

--- morpion/src/Joueur.java
+++ morpion/src/Joueur.java
@@ -1,13 +1,95 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Joueur {
 
     private String symbol;
 
-    public String getSymbol() {
-        return symbol;
+    private boolean isAI;
+
+    public Joueur(boolean isAI, String symbol) {
+        this.isAI = isAI;
+        this.symbol = symbol;
     }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public boolean isAI() {
+        return this.isAI;
+    }
+
+    public boolean humanPlay(PlateauDeJeu plateau, Scanner in) {
+        try {
+            System.out.println("Choissisez vos coordonnées (ex: 1,2) : \n");
+            String symbolJoueur = in.next();
+            String[] tokens = symbolJoueur.split(",");
+            while(tokens.length != 2) {
+                System.out.println("fait un effort... (EXEMPLEEEEEE : 1,2) : \n");
+                symbolJoueur = in.next();
+                tokens = symbolJoueur.split(",");
+            }
+            while(!plateau.addSymbol(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]), this.symbol)) {
+                System.out.println("Choissisez vos coordonnées (ex: 1,2) : \n");
+                symbolJoueur = in.next();
+                tokens = symbolJoueur.split(",");
+            }
+
+            int x = Integer.parseInt(tokens[0]);
+            int y = Integer.parseInt(tokens[1]);
+            if(plateau.win(x,y)) {
+                System.out.println("Tu as gagné la partie");
+                System.exit(0);
+            }
+            if(plateau.full()) {
+                System.out.println("MATCH NUUUUUUUL");
+                System.exit(0);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean aiPlay(PlateauDeJeu plateau, RemoteCommand commandAI) throws IOException {
+        try {
+            if(commandAI.sentPlateauToAI(plateau)) {
+                System.out.println("L'IA joue...\n");
+                //symbolJoueur = in.next();
+                String symbolJoueur = commandAI.receiptDataFromAI();
+                String[] tokens = symbolJoueur.split(",");
+                while(tokens.length != 2) {
+                    System.out.println("fait un effort... (EXEMPLEEEEEE : 1,2) : \n");
+                    //symbolJoueur = in.next();
+                    symbolJoueur = commandAI.receiptDataFromAI();
+                    tokens = symbolJoueur.split(",");
+                }
+                while(!plateau.addSymbol(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]), this.symbol)) {
+                    System.out.println("L'IA joue...\n");
+                    //symbolJoueur = in.next();
+                    symbolJoueur = commandAI.receiptDataFromAI();
+                    tokens = symbolJoueur.split(",");
+                }
+                int x = Integer.parseInt(tokens[0]);
+                int y = Integer.parseInt(tokens[1]);
+                if(plateau.win(x,y)) {
+                    System.out.println("L'IA a gagné la partie");
+                    System.exit(0);
+                }
+                if(plateau.full()) {
+                    System.out.println("MATCH NUUUUUUUL");
+                    System.exit(0);
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
     }
 
 }
--- morpion/src/Morpion.java
+++ morpion/src/Morpion.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Morpion {
 
-    private static Joueur joueur1 = new Joueur();
-    private static Joueur joueur2 = new Joueur();
+    private static Joueur joueur1 = null;
+    private static Joueur joueur2 = null;
     private static Scanner in = new Scanner(System.in);
     private static PlateauDeJeu plateau = new PlateauDeJeu();
     private static boolean partyFinish = false;
@@ -16,79 +16,48 @@ public class Morpion {
         // Tirage au sort
         String[] symbols = {"X" , "O"};
         int randomIndex = (int) Math.round( Math.random());
-        //joueur1.setSymbol(symbols[randomIndex]);
-        //joueur2.setSymbol(symbols[(randomIndex+1) % 2]);
-        joueur1.setSymbol("X");
-        joueur2.setSymbol("O");
-        System.out.println(symbols[randomIndex]);
-        System.out.println(symbols[(randomIndex+1) % 2]);
-
+        joueur1 = new Joueur(false, symbols[randomIndex]);
+        joueur2 = new Joueur(true, symbols[(randomIndex+1) % 2]);
+        //joueur1.setSymbol("X");
+        //joueur2.setSymbol("O");
         commandAI.initializeServer();
 
         while(!partyFinish) {
             if(joueur1.getSymbol().equalsIgnoreCase("X")) {
-                System.out.println("Joueur 1, choissisez vos coordonnées (ex: 1,2) : \n");
-                String symbolJoueur = in.next();
-                String[] tokens = symbolJoueur.split(",");
-                while(tokens.length != 2) {
-                    System.out.println("fait un effort... (EXEMPLEEEEEE : 1,2) : \n");
-                    symbolJoueur = in.next();
-                    tokens = symbolJoueur.split(",");
+                if(joueur1.isAI()) {
+                    joueur1.aiPlay(plateau,commandAI);
                 }
-                while(!plateau.addSymbol(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]), "X")) {
-                    System.out.println("Joueur 1, choissisez vos coordonnées (ex: 1,2) : \n");
-                    symbolJoueur = in.next();
-                    tokens = symbolJoueur.split(",");
+                else {
+                    joueur1.humanPlay(plateau, in);
                 }
                 //Verifier si le joueur1 a gagné ou si le plateau est plein
                 System.out.println(plateau.toString());
-                commandAI.sentPlateauToAI(plateau);
 
-                int x = Integer.parseInt(tokens[0]);
-                int y = Integer.parseInt(tokens[1]);
-                if(plateau.win(x,y)) {
-                    System.out.println("Le Joueur 1 a gagné la partie");
-                    break;
+                if(joueur2.isAI()) {
+                    joueur2.aiPlay(plateau,commandAI);
                 }
-                if(plateau.full()) {
-                    System.out.println("MATCH NUUUUUUUL");
-                    break;
+                else {
+                    joueur2.humanPlay(plateau, in);
                 }
-
-                if(commandAI.sentPlateauToAI(plateau)) {
-                    System.out.println("Joueur 2, choissisez vos coordonnées (ex: 1,2) : \n");
-                    //symbolJoueur = in.next();
-                    symbolJoueur = commandAI.receiptDataFromAI();
-                    tokens = symbolJoueur.split(",");
-                    while(tokens.length != 2) {
-                        System.out.println("fait un effort... (EXEMPLEEEEEE : 1,2) : \n");
-                        //symbolJoueur = in.next();
-                        symbolJoueur = commandAI.receiptDataFromAI();
-                        tokens = symbolJoueur.split(",");
-                    }
-                    while(!plateau.addSymbol(Integer.parseInt(tokens[0]),Integer.parseInt(tokens[1]), "O")) {
-                        System.out.println("Joueur 2 , choissisez vos coordonnées (ex: 1,2) : \n");
-                        //symbolJoueur = in.next();
-                        symbolJoueur = commandAI.receiptDataFromAI();
-                        tokens = symbolJoueur.split(",");
-                    }
-                    //Verifier si le joueur2 a gagné ou si le plateau est plein
-                    System.out.println(plateau);
-                    x = Integer.parseInt(tokens[0]);
-                    y = Integer.parseInt(tokens[1]);
-                    if(plateau.win(x,y)) {
-                        System.out.println("Le Joueur 2 a gagné la partie");
-                        break;
-                    }
-                    if(plateau.full()) {
-                        System.out.println("MATCH NUUUUUUUL");
-                        break;
-                    }
-                }
-
+                System.out.println(plateau.toString());
             }
             else {
-                System.out.println("Joueur 2, choissisez vos coordonnées\n");
+                if(joueur2.isAI()) {
+                    joueur2.aiPlay(plateau,commandAI);
+                }
+                else {
+                    joueur2.humanPlay(plateau, in);
+                }
+                //Verifier si le joueur1 a gagné ou si le plateau est plein
+                System.out.println(plateau.toString());
+
+                if(joueur1.isAI()) {
+                    joueur1.aiPlay(plateau,commandAI);
+                }
+                else {
+                    joueur1.humanPlay(plateau, in);
+                }
+                System.out.println(plateau.toString());
             }
 
         }
